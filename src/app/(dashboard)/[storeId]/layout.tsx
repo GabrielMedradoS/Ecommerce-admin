77b652b57fede514a7af7cbb3aecd8ee3f10251d
Gabriel Medrado <gabriel.medradoo@hotmail.com>
2023-12-04 16:06:33 -0300

    Combobox - Popover - Command and StoreSwitcher

--- src/app/(dashboard)/[storeId]/layout.tsx
+++ src/app/(dashboard)/[storeId]/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/navbar";
 import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
@@ -24,7 +25,7 @@ export default async function DashboardLayout({
 
   return (
     <>
-      <div>This will be Navbar</div>
+      <Navbar />
       {children}
     </>
   );
