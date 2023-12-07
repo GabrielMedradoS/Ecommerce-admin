0891f5dd1c1870cade74601fbb206efc2309c727
Gabriel Medrado <gabriel.medradoo@hotmail.com>
2023-12-06 21:14:13 -0300

    settings form and use-origin (url to paste and copy)

--- src/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -129,7 +129,7 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
       <Separator />
       <ApiAlert
         title="NEXT_PUBLIC_API_URL"
-        description="test-desc"
+        description={`${origin}]/api/${params.storeId}`}
         variant="public"
       />
     </>
