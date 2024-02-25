67deb25250c286f8b301ae1a976235c98bd32e63
Gabriel Medrado <gabriel.medradoo@hotmail.com>
2024-02-24 21:09:40 -0300

    Clerk css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import { dark, neobrutalism } from "@clerk/themes";
 
 import { ModalProvider } from "@/providers/modal-provider";
 import { ToasterProvider } from "@/providers/toast-provider";
